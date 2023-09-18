db4d0f3978c2f97b28ec8678d9e64d6cac17cbb8
AlekseiKostromin <cs777@yandex.ru>
2023-09-18 18:26:38 +0500

    Add homework_6 Add MixinBase

--- src/baseclass.py
+++ src/baseclass.py
@@ -10,5 +10,5 @@ class MixinBase:
 
     @classmethod
     def get_service(cls):
-        """возвращает объект для работы с YouTube API"""
+        """Возвращает объект для работы с YouTube API"""
         return build('youtube', 'v3', developerKey=cls.api_key)
--- src/video.py
+++ src/video.py
@@ -1,31 +1,31 @@
-import os
-from googleapiclient.discovery import build
+from src.baseclass import MixinBase
 
 
-class Video:
+class Video(MixinBase):
     """Класс для видео с Youtube"""
-    # API_KEY_YOUTUBE скопирован из гугла и вставлен в переменные окружения
-    api_key: str = os.getenv('API_KEY_YOUTUBE')
-
-    @classmethod
-    def get_service(cls):
-        """Возвращает объект для работы с YouTube API"""
-        return build('youtube', 'v3', developerKey=cls.api_key)
 
     def __init__(self, video_id: str):
-        """Экземпляр инициализируется по id видео. Дальше все данные будут подтягиваться по API."""
-        self.__video_id = video_id
+        """Экземпляр инициализируется по id видео и дальше все данные будут подтягиваться по API."""
+        try:
+            self.__video_id = video_id
+            self.video_response = (
+                self.get_service().videos().list(id=self.__video_id, part='snippet,statistics')).execute()
+            self.title: str = self.video_response['items'][0]['snippet']['title']
+            self.url: str = f"https://youtu.be/{self.__video_id}"
+            self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
 
-        self.video_response = (
-            self.get_service().videos().list(id=self.__video_id, part='snippet,statistics').execute())
-        self.video_title: str = self.video_response['items'][0]['snippet']['title']
-        self.url: str = f"https://youtu.be/{self.__video_id}"
-        self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+        except:
+            self.__video_id = video_id
+            self.title: str = None
+            self.url: str = None
+            self.view_count: int = None
+            self.like_count: int = None
 
-    def __str__(self):
-        """Представление класса для пользователя"""
-        return self.video_title
+
+def __str__(self):
+    """Представление класса для пользователя"""
+    return self.title
 
 
 class PLVideo(Video):
